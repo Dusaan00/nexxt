1a150ddec51efadc0be73900e9efd52e9e656d35
dusaan00 <dusa678@gmail.com>
2024-10-08 00:14:28 +0200

    Fixed basePath in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   output: "export",
   assetPrefix:
     process.env.NODE_ENV === "production" ? "https://www.ploty-grygov.cz" : "",
-  basePath: process.env.NODE_ENV === "production" ? "t" : "",
   images: {
     unoptimized: true,
   },
