4916e9ea60045ca1113a22c04b8b1e8f13278e7c
dusaan00 <dusa678@gmail.com>
2024-10-08 02:12:26 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,21 @@
 const nextConfig = {
   output: "export",
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://www.ploty-grygov.cz" : "",
+    process.env.NODE_ENV === "production" ? "https://ploty-grygov.cz" : "",
   images: {
     unoptimized: true,
   },
+  headers: () => [
+    {
+      source: "/_next/static/media/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Origin",
+          value: "*",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
